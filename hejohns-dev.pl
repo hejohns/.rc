14de11ec0640ea05ab2525751f1461e0f9015fa1
hejohns <hejohns@umich.edu>
2020-08-22 21:20:12 -0400

    changed variable names to be more general
    got tunnelvisioned...

--- hejohns-dev.pl
+++ hejohns-dev.pl
@@ -14,12 +14,12 @@ my $updated_control_file = 'hejohns-dev';
 
 # generate hejohns-dev
 my $seen_SUB_FOR_DEPS = 0;
-open(my $hejohns_dev_ctl_fh, '< :encoding(UTF-8)', $equivs_control_template)
+open(my $ctl_fh, '< :encoding(UTF-8)', $equivs_control_template)
     or die "equivs control file '$equivs_control_template' unable to be read: $!.";
-open(my $hejohns_dev_txt_fh, '< :encoding(UTF-8)', $dep_list)
+open(my $dep_txt_fh, '< :encoding(UTF-8)', $dep_list)
     or die "list of dependencies '$dep_list' unable to be read: $!.";
-my $hejohns_dev; #store updated control file as string (it's short anyways...)
-while(<$hejohns_dev_ctl_fh>){
+my $output; #store updated control file as string (it's short anyways...)
+while(<$ctl_fh>){
     if(m/$SUB_FOR_DEPS/){
         chomp;
         if($seen_SUB_FOR_DEPS == 0){
@@ -29,17 +29,17 @@ while(<$hejohns_dev_ctl_fh>){
             die "'SUBS_FOR_DEPS must only' appear once.";
         }
         s/$SUB_FOR_DEPS//;
-        $hejohns_dev .= $_;
-        while(<$hejohns_dev_txt_fh>){
+        $output .= $_;
+        while(<$dep_txt_fh>){
             chomp;
-            $hejohns_dev .= "$_, ";
+            $output .= "$_, ";
         }
-        chop($hejohns_dev) eq ' ' or die 'malformed dep list.';
-        chop($hejohns_dev) eq ',' or die 'malformed dep list.';
-        $hejohns_dev .= "\n";
+        chop($output) eq ' ' or die 'malformed dep list.';
+        chop($output) eq ',' or die 'malformed dep list.';
+        $output .= "\n";
     }
     else{
-        $hejohns_dev .= $_;
+        $output .= $_;
     }
 }
 if(-e $updated_control_file){
@@ -53,9 +53,9 @@ if(-e $updated_control_file){
     }
 }
 PROCEED:
-open(my $hejohns_dev_fh, '> :encoding(UTF-8)', $updated_control_file)
+open(my $output_fh, '> :encoding(UTF-8)', $updated_control_file)
     or die "unable to write to $updated_control_file: $!.";
-print $hejohns_dev_fh $hejohns_dev;
+print $output_fh $output;
 
 # call equivs-build
 !system "equivs-build $updated_control_file" or die "equivs probably isn't installed.";
