bf8a3d4fb26fc6aac30b5d5b64a427a3eb83ec57
hejohns <hejohns@umich.edu>
2020-08-22 21:09:43 -0400

    first actual use of perl for me

--- hejohns_dev.pl
+++ hejohns_dev.pl
@@ -7,30 +7,55 @@ use strict;
 use utf8;
 
 # hardcoded
-my $equivs_control = 'hejohns_dev';
+my $equivs_control_template = 'hejohns_dev.ctl';
 my $SUB_FOR_DEPS = 'SUB_FOR_DEPS';
 my $dep_list = 'hejohns_dev.txt';
+my $updated_control_file = 'hejohns_dev';
 
-open(my $hejohns_dev_fh, '<', $equivs_control)
-    or die "equivs control file \'$equivs_control\' unable to be read: $!";
-open(my $hejohns_dev_txt_fh, '<', $dep_list)
-    or die "list of dependencies \'$dep_list\' unable to be read: $!";
-my $updated_hejohns_dev;
-while(<$hejohns_dev_fh>){
+# generate hejohns_dev
+my $seen_SUB_FOR_DEPS = 0;
+open(my $hejohns_dev_ctl_fh, '< :encoding(UTF-8)', $equivs_control_template)
+    or die "equivs control file '$equivs_control_template' unable to be read: $!.";
+open(my $hejohns_dev_txt_fh, '< :encoding(UTF-8)', $dep_list)
+    or die "list of dependencies '$dep_list' unable to be read: $!.";
+my $hejohns_dev; #store updated control file as string (it's short anyways...)
+while(<$hejohns_dev_ctl_fh>){
     if(m/$SUB_FOR_DEPS/){
         chomp;
+        if($seen_SUB_FOR_DEPS == 0){
+            $seen_SUB_FOR_DEPS = 1;
+        }
+        else{
+            die "'SUBS_FOR_DEPS must only' appear once.";
+        }
         s/$SUB_FOR_DEPS//;
-        $updated_hejohns_dev = $updated_hejohns_dev . $_;
+        $hejohns_dev .= $_;
         while(<$hejohns_dev_txt_fh>){
             chomp;
-            $updated_hejohns_dev .= "$_, ";
+            $hejohns_dev .= "$_, ";
         }
-        chop($updated_hejohns_dev) eq ' ' or die 'malformed dep list';
-        chop($updated_hejohns_dev) eq ',' or die 'malformed dep list';
-        $updated_hejohns_dev .= "\n";
+        chop($hejohns_dev) eq ' ' or die 'malformed dep list.';
+        chop($hejohns_dev) eq ',' or die 'malformed dep list.';
+        $hejohns_dev .= "\n";
     }
     else{
-        $updated_hejohns_dev .= $_;
+        $hejohns_dev .= $_;
     }
 }
-print $updated_hejohns_dev;
+if(-e $updated_control_file){
+    say "'$updated_control_file' already exists. rm? y/n/c";
+    chomp(my $ync = <STDIN>);
+    if($ync eq 'y'){
+        # goto proceed
+    }
+    else{
+        die "aborting.";
+    }
+}
+PROCEED:
+open(my $hejohns_dev_fh, '> :encoding(UTF-8)', $updated_control_file)
+    or die "unable to write to $updated_control_file: $!.";
+print $hejohns_dev_fh $hejohns_dev;
+
+# call equivs-build
+!system "equivs-build $updated_control_file" or die "equivs probably isn't installed.";
