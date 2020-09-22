44207a009ce02ff32d381ccf0e7dc2607ef0a208
Johnson He <hejohns@umich.edu>
2020-09-22 16:31:17 -0400

    cleaned install.pl
    --made function calls look more functional programming like w/ spaces
    --made which files that about to be touched more clear

--- rc/install.pl
+++ rc/install.pl
@@ -14,34 +14,61 @@ sub filter_out_non_rcs{
     my @rcs_only;
     while(@_){
         my $file_to_consider = shift;
-        if(substr($file_to_consider, 0, 1) eq '.'){
+        if(substr ($file_to_consider, 0, 1) eq '.'){
             # kill . .. and any hidden files
             next;
         }
-        elsif($file_to_consider eq basename($0)){
+        elsif($file_to_consider eq basename ($0)){
             # also kill script name
             next;
         }
         else{
-            push(@rcs_only, ($file_to_consider));
+            push (@rcs_only, $file_to_consider);
         }
     }
     return @rcs_only;
 }
 
-opendir(my $cd_dh, dirname($0)) or die "Couldn't open rc file directory: $!";
+opendir (my $cd_dh, dirname($0)) or die "Couldn't open rc file directory: $!";
 my $home = File::HomeDir->my_home;
-say "Continue, potentially overwriting existing .rc files in $home? y/n";
-chomp(my $yn = <STDIN>);
-if($yn eq 'y'){
-    # continue;
+my @files_to_overwrite;
+foreach (&filter_out_non_rcs (readdir $cd_dh)){
+    my $diff_ret_val = system ('diff',
+            File::Spec->catfile($home, '.' . $_),
+            File::Spec->catfile(dirname($0), $_)
+            );
+    if ($diff_ret_val == 0){
+        next;
+    }
+    elsif ($diff_ret_val == -1){
+        die "diff not found. $!";
+    }
+    else{
+        push (@files_to_overwrite,
+                $_
+                );
+    }
+}
+if (@files_to_overwrite){
+    say "Continue, overwriting:";
+    foreach (@files_to_overwrite){
+        say File::Spec->catfile($home, '.' . $_);
+    }
+    say "? y/n";
+    chomp (my $yn = <STDIN>);
+    if($yn eq 'y'){
+        # continue;
+    }
+    else{
+        die("Aborting...\n");
+    }
+    foreach (@files_to_overwrite){
+        copy(File::Spec->catfile(dirname($0), $_),
+            File::Spec->catfile($home, '.' . $_)
+            )
+            or die "Failed to copy rc files over: $!";
+    }
 }
 else{
-    die("Aborting...\n");
+    say "No modifications required";
 }
-foreach(&filter_out_non_rcs(readdir($cd_dh))){
-    copy(File::Spec->catfile(dirname($0), $_),
-        File::Spec->catfile($home, '.' . $_))
-        or die "Failed to copy rc files over: $!";
-}
-say "Installed rc files to $home.";
