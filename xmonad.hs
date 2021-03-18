bde4a762f17789895dea6ffa8bae1e18ae42b44c
Johnson He <hejohns@umich.edu>
2021-03-18 05:19:09 -0400

    fix fullscreen border

--- xmonad.hs
+++ xmonad.hs
@@ -1,7 +1,12 @@
 import XMonad
 import XMonad.Config.Xfce
+import XMonad.Layout.NoBorders
 
 main = xmonad xfceConfig
-    {
-        terminal = "gnome-terminal"
+    { terminal = "gnome-terminal"
+    , layoutHook = myLayoutHook
+    , focusedBorderColor = "#800080"
+    , borderWidth = 1
     }
+    where
+    myLayoutHook = smartBorders $ layoutHook xfceConfig
