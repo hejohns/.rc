847d7529076877510e75673f4f300e84906d6648
hejohns <hejohns@umich.edu>
2022-01-04 13:09:13 -0500

    finally got xmonad + xinerama to work
    have the default "greedy" multiple screen behavior rn
    will probably want to change but haven't immediately taken a liking to
    anything else

--- xmonad.hs
+++ xmonad.hs
@@ -1,12 +1,20 @@
 import XMonad
 import XMonad.Config.Xfce
 import XMonad.Layout.NoBorders
+import XMonad.Actions.CycleWS
+import XMonad.Util.EZConfig
 
-main = xmonad xfceConfig
+main = xmonad $ xfceConfig
     { terminal = "xfce4-terminal"
     , layoutHook = myLayoutHook
     , focusedBorderColor = "#800080"
     , borderWidth = 1
     }
+    `additionalKeysP`
+    [ ("M-d", nextScreen)
+    , ("M-a", prevScreen)
+    , ("M-S-d", shiftNextScreen)
+    , ("M-S-a", shiftPrevScreen)
+    ]
     where
     myLayoutHook = smartBorders $ layoutHook xfceConfig
