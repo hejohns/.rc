a39c7af48e432f3aa1564da2f83e574a00c33494
hejohns <hejohns@umich.edu>
2023-11-06 03:28:47 -0500

    add xmonad Grid layout

--- xmonad.hs
+++ xmonad.hs
@@ -1,21 +1,23 @@
 import XMonad
-import XMonad.Config.Xfce
-import XMonad.Layout.NoBorders
 import XMonad.Actions.CycleWS
+import XMonad.Config.Xfce
+import XMonad.Layout.Grid
+import XMonad.Layout.NoBorders
 import XMonad.Util.EZConfig
 
-main = xmonad $ xfceConfig
-    { terminal = "xfce4-terminal"
-    , layoutHook = myLayoutHook
-    , focusedBorderColor = "#800080"
-    , borderWidth = 1
-    , modMask = mod4Mask
-    }
-    `additionalKeysP`
-    [ ("M-d", nextScreen)
-    , ("M-a", prevScreen)
-    , ("M-S-d", shiftNextScreen)
-    , ("M-S-a", shiftPrevScreen)
-    ]
-    where
-    myLayoutHook = smartBorders $ layoutHook xfceConfig
+main =
+  xmonad
+    $ xfceConfig
+        { terminal = "xfce4-terminal"
+        , layoutHook = myLayoutHook
+        , focusedBorderColor = "#800080"
+        , borderWidth = 1
+        , modMask = mod4Mask
+        }
+        `additionalKeysP` [ ("M-d", nextScreen)
+                          , ("M-a", prevScreen)
+                          , ("M-S-d", shiftNextScreen)
+                          , ("M-S-a", shiftPrevScreen)
+                          ]
+  where
+    myLayoutHook = Grid ||| (smartBorders $ layoutHook xfceConfig)
