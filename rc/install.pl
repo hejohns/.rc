c6b72a91e182bf201d9c48ed9c171c3262060c69
hejohns <hejohns@umich.edu>
2024-08-03 10:37:39 -0400

    make the install script usable again

--- rc/install.pl
+++ rc/install.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl
-use v5.28;
+use v5.32;
 use utf8;
 
 use warnings;
@@ -9,73 +9,80 @@ use File::Basename;
 use File::Copy;
 use File::Spec;
 
-sub filter_out_non_rcs{
-    my @rcs_only;
-    while (@_){
-        my $file_to_consider = shift;
-        if(substr ($file_to_consider, 0, 1) eq '.'){
-            # kill . .. and any hidden files
-            next;
-        }
-        elsif($file_to_consider eq basename ($0)){
-            # also kill script name
-            next;
-        }
-        elsif(-d $file_to_consider){
-            # and skip directories
-            # TODO: will handle this sometime...
-            next;
-        }
-        else{
-            push (@rcs_only, $file_to_consider);
-        }
-    }
-    return @rcs_only;
-}
-
 $SIG{INT} = sub{die "Aborting...\n"};
+
 opendir (my $cd_dh, dirname($0)) or die "Couldn't open rc file directory: $!";
-my $home = `echo ~`;
-if($? != 0){
-    die "$!";
-}
+my $home = $ENV{HOME} // die "$!";
 chomp $home;
-$home = $home . '/';
-my @files_to_overwrite;
-foreach (&filter_out_non_rcs (readdir $cd_dh)){
-    my ($here, $there) = (File::Spec->catfile($home, '.' . $_),
-        File::Spec->catfile(dirname($0), $_)
-        );
-    `diff $here $there`;
-    if ($? == -1){
+
+my @upstream_rel = `find . -type f ! -name '.*' ! -name '${\(basename($0))}'`;
+chomp @upstream_rel;
+my @downstream_rel = map {'./.' . substr $_, 2} @upstream_rel;
+my @zipped = map {($upstream_rel[$_] , $downstream_rel[$_])} (0 .. $#upstream_rel);
+my @zipped_diff;
+while (@zipped){
+    my $upstream_rel = shift @zipped;
+    my $downstream_rel = shift @zipped;
+    my $upstream_abs = File::Spec->catfile(dirname($0), $upstream_rel);
+    my $downstream_abs = File::Spec->catfile($home, $downstream_rel);
+    `diff $upstream_abs $downstream_abs`;
+    if($? == -1){
         die "diff not found. $!";
     }
-    elsif ($? >> 8 == 0){
+    elsif($? >> 8 == 0){
         next;
     }
     else{
-        push (@files_to_overwrite, $_);
+        push @zipped_diff, $upstream_abs, $downstream_abs;
     }
 }
-if (@files_to_overwrite){
-    foreach (@files_to_overwrite){
-        print "overwrite: ";
-        print File::Spec->catfile($home, '.' . $_);
-        say "? y/n";
-        chomp (my $yn = <STDIN>);
-        unless ($yn eq 'y'){
-            say "skipping...";
-            next;
-        }
-        copy (File::Spec->catfile(dirname($0), $_),
-            File::Spec->catfile($home, '.' . $_)
-        )
-            or die "Failed to copy rc files over: $!";
+say "No modifications required" if !@zipped_diff;
+while (@zipped_diff){
+    my $upstream_abs = shift @zipped_diff;
+    my $downstream_abs = shift @zipped_diff;
+    print "overwrite: ";
+    print $downstream_abs;
+    RETRY:
+    say "? y/n/d(iff)/(pu)s(h)/(u)p(load diff)";
+    my $yn = <STDIN> // die "eof on stdin: $!";;
+    chomp($yn);
+    if($yn eq 'y'){
+        say $upstream_abs, $downstream_abs;
+        #copy($upstream_abs, $downstream_abs) or die "Failed to copy $_ : $!";
     }
+    elsif($yn eq 'n'){
+        say "skipping...";
+        next;
+    }
+    elsif($yn eq 'd'){
+        say '################################################################################';
+        # this order makes more intuitive sense when reading the program output
+        print `diff -NU 5 $downstream_abs $upstream_abs`;
+        say '################################################################################';
+    }
+    elsif($yn eq 's'){
+        say $downstream_abs, $upstream_abs;
+        #copy($downstream_abs, $upstream_abs) or die "Failed to copy $_ : $!";
+    }
+    elsif($yn eq 'p'){
+        say '################################################################################';
+        print `diff -NU 5 $upstream_abs $downstream_abs`;
+        say '################################################################################';
+    }
+    goto RETRY;
 }
-else{
-    say "No modifications required";
+
+say '$ git';
+`git add -u`;
+print `git -c color.ui=always status`;
+say 'push changes? y/n';
+my $yn = <STDIN> // die "eof on stdin: $!";
+chomp($yn);
+if($yn eq 'y'){
+    system 'git', 'commit';
 }
+exit 0;
+
 # mutt stuff
 my $mutt_home = File::Spec->catfile($home, ".mutt");
 if (-e $mutt_home){
@@ -114,6 +121,7 @@ if (-e $mutt_home){
 else{
     say "~/.mutt does not exist: $!"
 }
+
 # bash completion
 my $completions = $home . '.nix-profile/share/bash-completion/completions';
 if(-e $completions && -d $completions){
